adf3d0dc528d33f3878b163fe8ef1d629a156f13
Indigowar <indigowar.github@gmail.com>
2022-07-14 17:43:17 +0300

    Write ArticleRepository interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -18,3 +18,15 @@ type UserRepository interface {
 	ChangeEmail(uuid.UUID, string) error
 	ChangePassword(uuid.UUID, string) error
 }
+
+var (
+	ErrArticleAlreadyExists = errors.New("error: article already exists")
+	ErrArticleNotFound      = errors.New("error: article was not found")
+)
+
+type ArticleRepository interface {
+	Get(uuid.UUID) (domain.Article, error)
+	Create(domain.Article) error
+	Delete(uuid.UUID) error
+	Update(uuid.UUID, string, string) error
+}
