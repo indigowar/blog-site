b10f773eba1df456e65e57cfc155002a134532fa
Indigowar <indigowar.github@gmail.com>
2022-07-15 17:41:42 +0300

    Add test of NewJWT and Verify

--- pkg/auth/token_manager_test.go
+++ pkg/auth/token_manager_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"github.com/google/uuid"
 	"testing"
 	"time"
 )
@@ -26,5 +27,24 @@ func TestNewTokenManager(t *testing.T) {
 	_, err = NewTokenManager(validSecret, time.Hour, time.Second*24)
 	if err == nil {
 		t.Errorf("NewTokenManager got invalid set of arguments but didn't return the error")
+	}
+}
+
+func TestTokenManager(t *testing.T) {
+	manager, _ := NewTokenManager([]byte("hello, world"), time.Hour, 5*time.Minute)
+
+	id := uuid.New()
+	token, err := manager.NewJWT(id)
+	if err != nil {
+		t.Errorf("TokenManager.NewJWT got an error with valid args:%s", err)
+	}
+
+	tokenId, _, err := manager.Verify(token)
+	if err != nil {
+		t.Errorf("TokenManager.Verify got an error with valid args: %s", err)
+	}
+
+	if tokenId != id {
+		t.Errorf("The extracted id from token is not the same with input")
 	}
 }
